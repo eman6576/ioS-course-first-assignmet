6863e6880384e88b4dab5c22ab0c4e954ba7fd58
Manny <Emanuel@emanuels-macbook-pro.local>
2016-01-15 22:10:59 -0500

    Initial Commit

---
+++ PlayingHideAndSeekWithColors/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PlayingHideAndSeekWithColors
+//
+//  Created by Emanuel  Guerrero on 1/15/16.
+//  Copyright © 2016 Project Omicron. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
